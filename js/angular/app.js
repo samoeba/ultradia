e90220ed18d2fb64b36e9bf99ac2503c302a3402
Sam Casey <samcaseydesign@gmail.com>
2015-10-09 11:12:13 -0500

    ui-router

--- js/angular/app.js
+++ js/angular/app.js
@@ -1,22 +1,25 @@
 var careersApp = angular.module("ultradiaCareers", ["ui.router", "services"]);
 
 
-careersApp.run(function($rootScope, $state) {
-    $rootScope.$scope = $state;
-});
-//careersApp.config(function($stateProvider, $locationProvider){
-//    $locationProvider.html5Mode({
-//        enabled: true,
-//        requireBase: false
-//    });
-//    $stateProvider
-//        .state("careers", {
-//            url: "/careers"
-//        }
-//        .state('careers.position', {
-//            url: "/position/:id",
-//            templateUrl: "position.html",
-//            controller: "PositionsController"
-//        });
-//    //$urlRouterProvider.otherwise('/');
+//careersApp.run(function($rootScope, $state) {
+//    $rootScope.$scope = $state;
 //});
+
+careersApp.config(function($stateProvider) {
+    //$locationProvider.html5Mode({
+    //    enabled: true,
+    //    requireBase: false
+    //});
+    $stateProvider
+        .state("home", {
+            url: "/careers",
+            templateUrl: "openings.html",
+            controller: "CareersController"
+        }
+        .state('careers.position', {
+            url: "/position/:id",
+            templateUrl: "position.html",
+            controller: "PositionsController"
+        });
+    //$urlRouterProvider.otherwise('/');
+});
