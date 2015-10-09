4e1c2bd76070d23a7bbbac449b57e9a5ad21b75c
Sam Casey <samcaseydesign@gmail.com>
2015-10-09 12:10:55 -0500

    ui-router

--- js/angular/controllers.js
+++ js/angular/controllers.js
@@ -4,16 +4,6 @@ angular.module("ultradiaCareers")
     .controller('CareersController', ["$scope", "GetOpenings", function ($scope, GetOpenings) {
         "use strict";
 
-        //GetOpenings.openings.then(function (positions) {
-        //    $scope.openings = positions.data;
-        //});
-
-        //$http({ method: 'GET', url: '/careers/positions.json' }).success(function (data) {
-        //    console.log("Made it this far.");
-        //    $scope.openings = data;
-        //    console.log($scope.openings);
-        //});
-
         console.log("Made it to here");
 
         GetOpenings.openings.then(function (positions) {
@@ -34,6 +24,7 @@ angular.module("ultradiaCareers")
             console.log($scope.openings);
             $scope.position = positions.data[GetOpenings.getPositionById($scope.openings, $stateParams.id)];
             console.log($scope.position);
+            console.log(GetOpenings.getPositionById($scope.openings, $stateParams.id));
         });
 
     }]);
