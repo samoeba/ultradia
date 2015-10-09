02461e458fc18e48a62c6dc5df6898492848f601
Sam Casey <samcaseydesign@gmail.com>
2015-10-09 12:14:42 -0500

    ui-router

--- js/angular/controllers.js
+++ js/angular/controllers.js
@@ -16,12 +16,9 @@ angular.module("ultradiaCareers")
     .controller('PositionsController', ["$scope", "$stateParams", "GetOpenings", function ($scope, $stateParams, GetOpenings) {
         "use strict";
 
-        console.log($stateParams.id);
-
         GetOpenings.openings.then(function (positions) {
-            console.log("PositionsCtrl test");
             $scope.openings = positions.data;
-            console.log($scope.openings);
+            console.log($scope.openings[0]);
             $scope.position = positions.data[GetOpenings.getPositionById($scope.openings, $stateParams.id)];
             console.log($scope.position);
             console.log(GetOpenings.getPositionById($scope.openings, $stateParams.id));
