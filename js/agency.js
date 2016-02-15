b497be6c9a3cca400bf26be4c92130e053179e5f
Sam Casey <samcaseydesign@gmail.com>
2016-02-14 23:42:43 -0600

    added ben

--- js/agency.js
+++ js/agency.js
@@ -18,7 +18,7 @@ $(function() {
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy ({
     offset: 200
-})
+});
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
