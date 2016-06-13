9a94db9bc80110a3168c7b097731ff977ee70d1b
Sam Casey <scasey@gobrandgo.com>
2016-06-13 00:04:49 -0500

    removed changing bg from mobile and added resizing logic

--- js/chrona-landing.js
+++ js/chrona-landing.js
@@ -1,16 +1,17 @@
 $(function() {
 
-    var bgImages, current, backgrounds, whiteOverlay, range;
+    var bgImages, bgTimer, current, backgrounds, soloBackground, whiteOverlay;
     bgImages = $(".bg-images");
     backgrounds = ["url('../img/bg_landing_bedroom.jpg')","url('../img/bg_landing_hospital.jpg')", "url('../img/bg_landing_hotel-male.jpg')", "url('../img/bg_landing_hotel-couple.jpg')"];
+    soloBackground = ["url('../img/bg_landing_bedroom.jpg')"];
     current = 0;
 
     function nextBackground() {
         bgImages.css( "background-image", backgrounds[current = ++current % backgrounds.length] );
-        setTimeout(nextBackground, 5000);
+        bgTimer = setTimeout(nextBackground, 5000);
     }
-    setTimeout(nextBackground, 5000);
 
+    bgImages.css( "background-image", backgrounds[0] );
 
     whiteOverlay = $(".white-overlay");
 
@@ -29,6 +30,37 @@ $(function() {
 
     });
 
-    bgImages.css( "background-image", backgrounds[0] );
+    /*! ^#--Header Resizing Stuff */
+    var lastWinWidth = $(window).width();
+    var lastWinHeight = $(window).height();
+
+    $(window).resize(function() {
+
+        var curWinWidth = $(window).width();
+        var curWinHeight = $(window).height();
+
+
+        //if we went from desktop to mobile, close the slide out menu
+        if (lastWinWidth > 960 && curWinWidth <= 960) {
+            clearTimeout(bgTimer);
+        }
+
+        //if we went from mobile to desktop, close the slide out menu
+        if (lastWinWidth <= 960 && curWinWidth > 960) {
+            setTimeout(nextBackground, 5000);
+        }
+
+        lastWinHeight = curWinHeight;
+        lastWinWidth = curWinWidth;
+
+    });
+    // End Header Resizing Stuff
+
+    if (lastWinWidth > 960) {
+        //bgImages.css( "background-image", soloBackground[current = ++current % backgrounds.length] );
+        setTimeout(nextBackground, 5000);
+    }
+
+
 
 });
